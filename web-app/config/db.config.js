3b1986c627873344216817535d7d671521c6505f
Mohd Ainal Farhan Mohamad Johari <56626447+Ainal-Farhan@users.noreply.github.com>
2020-12-26 08:35:21 +0800

    Update db.config.js

--- web-app/config/db.config.js
+++ web-app/config/db.config.js
@@ -1,28 +1,28 @@
-// module.exports = {
-//     HOST: "johnny.heliohost.org",
-//     USER: "ainalfa_ainal3",
-//     PASSWORD: "ainal3@123",
-//     DB: "ainalfa_go-bites-db",
-//     dialect: "mysql",
-//     pool: {
-//         max: 9999,
-//         min: 0,
-//         acquire: 30000,
-//         idle: 10000
-//     }
-// };
-
-
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "@A1n4l990311",
-    DB: "gobites",
+    HOST: "johnny.heliohost.org",
+    USER: "ainalfa_ainal3",
+    PASSWORD: "ainal3@123",
+    DB: "ainalfa_go-bites-db",
     dialect: "mysql",
     pool: {
-        max: 10,
+        max: 9999,
         min: 0,
         acquire: 30000,
         idle: 10000
     }
 };
+
+
+// module.exports = {
+//     HOST: "localhost",
+//     USER: "root",
+//     PASSWORD: "@A1n4l990311",
+//     DB: "gobites",
+//     dialect: "mysql",
+//     pool: {
+//         max: 10,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000
+//     }
+// };
