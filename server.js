b52cbf5fd6a030db2064bd8c4fade7fc1c09d369
Mohd Ainal Farhan Mohamad Johari <56626447+Ainal-Farhan@users.noreply.github.com>
2021-04-10 16:55:18 +0800

    Update server.js

--- server.js
+++ server.js
@@ -17,14 +17,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const db = require("./web-app/models/index.js");
 
-// Drop existing table and re-sync database
-db.sequelize.sync({ force: true }).then(() => {
-        console.log("Drop and re-sync db.");
-    });
+// // Drop existing table and re-sync database
+// db.sequelize.sync({ force: true }).then(() => {
+//         console.log("Drop and re-sync db.");
+//    });
 
-// db.sequelize.sync().then(() => {
-//     console.log("sync db status: done");
-// });
+ db.sequelize.sync().then(() => {
+     console.log("sync db status: done");
+ });
 
 // simple route
 app.get("/", (req, res) => {
